8df55dd6db74e18b7b31e75498cf5fc06a2ee69c
juaxE <juho.kirvesmaki@helsinki.fi>
2024-02-20 23:25:28 +0200

    4.7

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
     let length = blogs.length
 
@@ -13,6 +15,10 @@ const totalLikes = (blogs) => {
 }
 
 const favoriteBlog = (blogs) => {
+    if (Object.keys(blogs).length === 0) {
+        return 'No blogs or likes'
+    }
+
     const reducer = (current, candidate) => {
         const { likes: likesCurrent } = current
         const { likes: likesCandidate } = candidate
@@ -20,15 +26,41 @@ const favoriteBlog = (blogs) => {
     }
     const result = blogs.reduce(reducer, {})
 
-    if (Object.keys(result).length === 0) {
-        return 'No blogs or likes'
-    }
+
     const { title, author, likes } = result
     return { title, author, likes }
+}
+
+const mostBlogs = (blogs) => {
+    if (Object.keys(blogs).length === 0) {
+        return 'No blogs or likes'
+    }
+
+    const blogsByAuthor = _.groupBy(blogs, 'author')
+
+    const authorWithMostBlogs = _.maxBy(Object.keys(blogsByAuthor), (author) => blogsByAuthor[author].length)
+    const numberOfBlogs = blogsByAuthor[authorWithMostBlogs].length
+
+    return { author: authorWithMostBlogs, blogs: numberOfBlogs }
+}
+
+const mostLikedAuthor = (blogs) => {
+    if (Object.keys(blogs).length === 0) {
+        return 'No blogs or likes'
+    }
+
+    const blogsByAuthor = _.groupBy(blogs, 'author')
+
+    const authorWithMostLikes = _.maxBy(Object.keys(blogsByAuthor), (author) => _.sumBy(blogsByAuthor[author], 'likes'))
+    const numberOfLikes = _.sumBy(blogsByAuthor[authorWithMostLikes], 'likes')
+
+    return { author: authorWithMostLikes, likes: numberOfLikes }
 }
 
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikedAuthor
 }
