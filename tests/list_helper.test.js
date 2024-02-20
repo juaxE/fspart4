2ab99cca5f9d5c5dc176eb4678d172025b9dfb34
juaxE <juho.kirvesmaki@helsinki.fi>
2024-02-20 22:42:01 +0200

    4.5

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -2,6 +2,42 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
+const listWithOneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }
+]
+const listWithNoBlog = []
+const listWithManyBlogs = [{
+  _id: "5a422a851b54a676234d17f7",
+  title: "React patterns",
+  author: "Michael Chan",
+  url: "https://reactpatterns.com/",
+  likes: 7,
+  __v: 0
+},
+{
+  _id: "5a422aa71b54a676234d17f8",
+  title: "Go To Statement Considered Harmful",
+  author: "Edsger W. Dijkstra",
+  url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+  likes: 5,
+  __v: 0
+},
+{
+  _id: "5a422b3a1b54a676234d17f9",
+  title: "Canonical string reduction",
+  author: "Edsger W. Dijkstra",
+  url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+  likes: 12,
+  __v: 0
+}
+]
 
 test('dummy returns one', () => {
   const blogs = []
@@ -11,42 +47,6 @@ test('dummy returns one', () => {
 })
 
 describe('total likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    }
-  ]
-  const listWithNoBlog = []
-  const listWithManyBlogs = [{
-    _id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: "5a422b3a1b54a676234d17f9",
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-    __v: 0
-  }
-  ]
 
   test('when list has only one blog equals the likes of that', () => {
     const result = listHelper.totalLikes(listWithOneBlog)
@@ -59,6 +59,29 @@ describe('total likes', () => {
   test('when there is many blogs', () => {
     const result = listHelper.totalLikes(listWithManyBlogs)
     assert.strictEqual(result, 24)
+  })
+})
+
+describe('favorite blog', () => {
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    assert.deepStrictEqual(result, {
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      likes: 5
+    })
+  })
+  test('when there is no blogs', () => {
+    const result = listHelper.favoriteBlog(listWithNoBlog)
+    assert.strictEqual(result, 'No blogs or likes')
+  })
+  test('when there is many blogs', () => {
+    const result = listHelper.favoriteBlog(listWithManyBlogs)
+    assert.deepStrictEqual(result, {
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      likes: 12
+    })
   })
 })
 
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -12,7 +12,23 @@ const totalLikes = (blogs) => {
     return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    const reducer = (current, candidate) => {
+        const { likes: likesCurrent } = current
+        const { likes: likesCandidate } = candidate
+        return likesCurrent >= likesCandidate ? current : candidate
+    }
+    const result = blogs.reduce(reducer, {})
+
+    if (Object.keys(result).length === 0) {
+        return 'No blogs or likes'
+    }
+    const { title, author, likes } = result
+    return { title, author, likes }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
