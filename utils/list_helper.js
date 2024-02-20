eb758d2199221825b1da6dd2b6c9cd488eebe1fb
juaxE <juho.kirvesmaki@helsinki.fi>
2024-02-20 22:14:46 +0200

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,6 +4,15 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const reducer = (sum, item) => {
+        const { likes } = item
+        return sum + likes
+    }
+    return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
