80e3a41b800804a7e3e062c256258ec5eb3dd36b
juaxE <juho.kirvesmaki@helsinki.fi>
2024-02-26 17:05:11 +0200

    4.9

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -9,52 +9,41 @@ const api = supertest(app)
 
 const initialBlogs = [
     {
-        _id: "5a422a851b54a676234d17f7",
+
         title: "React patterns",
         author: "Michael Chan",
         url: "https://reactpatterns.com/",
         likes: 7,
-        __v: 0
     },
     {
-        _id: "5a422aa71b54a676234d17f8",
         title: "Go To Statement Considered Harmful",
         author: "Edsger W. Dijkstra",
         url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
         likes: 5,
-        __v: 0
     },
     {
-        _id: "5a422b3a1b54a676234d17f9",
         title: "Canonical string reduction",
         author: "Edsger W. Dijkstra",
         url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
         likes: 12,
-        __v: 0
     },
     {
-        _id: "5a422b891b54a676234d17fa",
         title: "First class tests",
         author: "Robert C. Martin",
         url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
         likes: 10,
-        __v: 0
     },
     {
-        _id: "5a422ba71b54a676234d17fb",
         title: "TDD harms architecture",
         author: "Robert C. Martin",
         url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
         likes: 0,
-        __v: 0
     },
     {
-        _id: "5a422bc61b54a676234d17fc",
         title: "Type wars",
         author: "Robert C. Martin",
         url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
         likes: 2,
-        __v: 0
     }
 ]
 
@@ -74,7 +63,7 @@ test('blogs are returned as json', async () => {
         .expect('Content-Type', /application\/json/)
 })
 
-test('there are the right amount of blogs', async () => {
+test('there is the right amount of blogs', async () => {
     const response = await api.get('/api/blogs')
     assert.strictEqual(response.body.length, initialBlogs.length)
 })
@@ -84,6 +73,13 @@ test('the titles include one about React patterns', async () => {
 
     const titles = response.body.map(e => e.title)
     assert(titles.includes('React patterns'))
+
+})
+
+test('the id field is without underscore', async () => {
+    const response = await api.get('/api/blogs')
+    const firstBlog = response.body[0]
+    assert.ok(firstBlog.hasOwnProperty('id'), 'Expected field id')
 })
 
 after(async () => {
